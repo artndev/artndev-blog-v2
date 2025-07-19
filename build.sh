af2d064752647be5d8740f9fa9530405272cf601
artndev <97910780+artndev@users.noreply.github.com>
2025-07-19 19:37:15 +0300

    .

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ echo "[INFO] Removing previous Docker containers..."
 docker-compose down
 
 echo "[INFO] Clearing Docker system..."
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 docker system prune -a -f
 
 echo "[INFO] Rebuilding Docker containers..."
