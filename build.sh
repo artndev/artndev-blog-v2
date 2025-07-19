a4ba1d02958c34736048c69265e3597ecdf72472
artndev <97910780+artndev@users.noreply.github.com>
2025-07-19 13:38:46 +0300

    .

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+echo "Remove previous containers..."
 docker-compose down
+
+echo "Clear Docker system..."
+docker system prune -a -f
+
+echo "Rebuild Docker containers..."
 docker-compose build --no-cache
