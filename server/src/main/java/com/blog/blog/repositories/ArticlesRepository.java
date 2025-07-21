2961d0745556d2e0b329f5f3767a156e383519a2
artndev <97910780+artndev@users.noreply.github.com>
2025-07-21 16:04:55 +0300

    .

--- server/src/main/java/com/blog/blog/repositories/ArticlesRepository.java
+++ server/src/main/java/com/blog/blog/repositories/ArticlesRepository.java
@@ -177,6 +177,11 @@ public class ArticlesRepository implements ArticlesDao {
     @Transactional
     public void deleteArticle(Integer id) throws DataAccessException {
         jdbcTemplate.update(
+            "DELETE FROM ArticleTags WHERE ArticleId = ?;",
+            id
+        );
+
+        jdbcTemplate.update(
             """
                 DELETE FROM Articles WHERE Id = ?;
             """,
